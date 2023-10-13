936604a594e2d694b80fb920d2e17bab7efb35f7
gustavkis <gustavkis.bp@gmail.com>
2023-10-14 01:21:26 +0200

    tábla

--- Webshop/resources/views/show.blade.php
+++ Webshop/resources/views/show.blade.php
@@ -2,18 +2,45 @@
 @section('title', 'Kosár')
 
 @section('content')
-    <h2>Kosár tartalma</h2>
-    @if ($cartItems->isEmpty())
-        <p>A kosár üres.</p>
-    @else
-        <ul>
-            @foreach ($cartItems as $item)
-                <li>
-                    Termék neve: {{ $item->termek->name }}
-                    Mennyiség: {{ $item->quantity }}
-                    Ár: {{ $item->price }}
-                </li>
-            @endforeach
-        </ul>
-    @endif
+   
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <table class="table table-info table-striped table-hover">
+           <thead>
+
+           <tr>
+               <th>Termék neve:</th>
+               <th>Mennyiség</th>
+               <th>Ár/darab</th>
+               <th>Összesen</th>
+               <th></th>
+            </tr>
+           </thead>
+           <tbody>
+           @foreach ($cartItems as $item)
+           <tr>
+           <td>{{ $item->termek->name }}</td>
+           <td>{{ $item->quantity }}</td>
+           <td>{{ $item->termek->price }}</td>
+           <td> {{ $item->price }}</td>
+           <td>
+            <a href="" class="btn btn-sm btn-danger">Töröl</a>
+           </td>
+          
+           </tr>
+          @endforeach
+          </tbody>
+          <tfoot>
+            <th colspan="3"></th>
+            <th>Összesen:</th>
+            <th >1000  Ft</th>
+            
+         </tfoot>
+    </table>
+   </div>   
+</div>
+
+                    
 @endsection
